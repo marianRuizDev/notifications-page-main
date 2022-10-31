7b0f2ab07734bfd46f40ebff667f0429a39f1b42
mariangelkruiz <mariangel8ruiz@gmail.com>
2022-10-30 21:45:13 -0300

    notifications document

---
+++ src/utils/notifications.js
@@ -0,0 +1,45 @@
+const notifications = [
+    {
+        name: "Mark Webber",
+        content: "reacted to your recent post My first tournament today!",
+        date: "1m ago",
+        message: false
+    },
+    {
+        name: "Angela Gray",
+        content: "followed you",
+        date: "5m ago",
+        message: false
+    },
+    {
+        name: "Jacob Thompson",
+        content: "has joined your group Chess Club",
+        date: "1 day ago",
+        message: false
+    },
+    {
+        name: "Rizky Hasanuddin",
+        content: "sent you a private message",
+        date: "5 days ago",
+        message: true,
+        contentMessage: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game."
+    },
+    {
+        name: "Kimberly Smith",
+        content: "commented on your picture",
+        date: "1 week ago",
+        message: false
+    },
+    {
+        name: "Nathan Peterson",
+        content: "reacted to your recent post 5 end-game strategies to increase your win rate",
+        date: "2 weeks ago",
+        message: false
+    },
+    {
+        name: "Anna Kim",
+        content: "left the group Chess Club",
+        date: "2 weeks ago",
+    message: false        
+    }
+];
